963a2fd6d1498d2ef03e75a1a808de921ceae178
Chris3467 <chris.collado815@gmail.com>
2024-12-16 12:11:31 -0500

    finished

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Welcome() {
+  return (
+    <>
+      <h1>Pokemon App</h1>
+      <p>
+        You can search for a pokemon by name or id. If you are not sure of the
+        name, you can scroll through the list using the "back" and " next"
+        buttons. This list is on the "Find a Pokemon" page.
+      </p>
+    </>
+  );
+}
+
+export default Welcome;
